4a8508473ab5b712c5b287b332f55f43a4593533
Stefan Fürst <stefanfuerst@mac.com>
2020-08-25 19:21:25 +0200

    Localization

--- Dynamic Cost/ShippingSample.grandtotalplugin/index.js
+++ Dynamic Cost/ShippingSample.grandtotalplugin/index.js
@@ -5,6 +5,7 @@ function changed()
 	var result = {};
 	var pieces = query().valueForKeyPath("@sum.quantity");
 	result["cost"] = 6.5 * pieces;
+	result["quantity"] = 0;
 	result["unit"] = "";
 	result["notes"] = "" + pieces + " " + localize("items home delivery");
 	result["name"] = localize("ShippingSample");
