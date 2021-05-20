abb23218e038d2ae711ceb90d97aa3e762a1b5c3
Stefan Fuerst <stefanfuerst@mac.com>
2021-05-20 18:06:11 +0200

    Added Margin Calculator Sample

---
+++ Documents/Margin.grandtotalplugin/index.js
@@ -0,0 +1,39 @@
+/*
+	
+	
+*/
+
+
+calculate();
+
+
+function calculate()
+{
+	var aDocument = query().record();
+	var aItems = aDocument.valueForKey("children").records();
+	var aTable = [];
+	var purchasingPrice = 0;
+	var sellingPrice = 0;
+	for (aItemIndex in aItems)
+	{
+		var aTableItem = [];
+		aItem = aItems[aItemIndex];
+		
+		var aCustomValues = aItem.customValues.records()
+		for (aValueIndex in aCustomValues)
+		{
+			aValue = aCustomValues[aValueIndex];
+			if (aValue.valueForKeyPath("field.name") == localize("Purchasing Price"))
+			{
+			
+				purchasingPrice += aValue.name * aItem.quantity;
+				sellingPrice += aItem.unitPrice * aItem.quantity;
+				break;
+			}
+		}
+	}
+	
+	displayUserNotification(localize("Margin"), aDocument.valueForKeyPath("currency.name")+ " " + (sellingPrice - purchasingPrice));
+	
+}
+
